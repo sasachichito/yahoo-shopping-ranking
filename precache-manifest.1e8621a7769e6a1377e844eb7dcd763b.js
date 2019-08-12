cf5d410ccdf3f2a2c97dbd1aabf2a3c2da03bd0f
sasachichito <sasachichito@yahoo.co.jp>
2019-08-12 18:04:07 +0900

    Updates

---
+++ precache-manifest.1e8621a7769e6a1377e844eb7dcd763b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a6027e67be3966c8bcc854731b18d5",
+    "url": "/yahoo-shopping-ranking/index.html"
+  },
+  {
+    "revision": "6123f408cacbe6a03ea1",
+    "url": "/yahoo-shopping-ranking/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "91e2861efcd2d83069f1",
+    "url": "/yahoo-shopping-ranking/static/js/2.00f77669.chunk.js"
+  },
+  {
+    "revision": "6123f408cacbe6a03ea1",
+    "url": "/yahoo-shopping-ranking/static/js/main.e6c5c456.chunk.js"
+  },
+  {
+    "revision": "0cb1a22cd4d3761b4165",
+    "url": "/yahoo-shopping-ranking/static/js/runtime~main.888bc89d.js"
+  }
+]);
